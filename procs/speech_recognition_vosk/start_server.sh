6c4010bf52478d037bb175b12d948675846c7097
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-05-21 17:44:50 +0900

    バックグラウンドで起動

--- procs/speech_recognition_vosk/start_server.sh
+++ procs/speech_recognition_vosk/start_server.sh
@@ -2,7 +2,7 @@
 
 MODEL=${1:-"models/vosk-model-small-ja-0.22"}
 
-docker run  --name vosk --rm \
+docker run -d --name vosk --rm \
   -p 2700:2700 \
   -v $(pwd)/models:/opt/vosk-server/models \
   -w /opt/vosk-server/websocket \
