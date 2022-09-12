0a686e21d25117888f95ebec6a5b62d2101c77c4
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-09-12 21:08:40 +0900

    StableDiffusionでの複数サンプル対応

--- procs/text_image_generation_stable_diffusion/run.py
+++ procs/text_image_generation_stable_diffusion/run.py
@@ -4,9 +4,11 @@ TOKEN = os.environ['HUGGINGFACE_TOKEN']
 
 parser = argparse.ArgumentParser()
 parser.add_argument("prompt", type=str, default="sunset over a lake in the mountains")
+parser.add_argument("num_of_samples", type=int, default=0)
 parser.add_argument("outpath",type=str, default="out")
 args = parser.parse_args()
 prompt = args.prompt
+num_of_samples = args.num_of_samples
 outpath = args.outpath
 
 
@@ -15,8 +17,10 @@ from diffusers import StableDiffusionPipeline
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=TOKEN)
 pipe.to("cuda")
 
-image = pipe(prompt)["sample"][0]
-image.save(f"{outpath}.png")
+images = pipe([prompt] * num_of_samples)["sample"]
+for i in range(num_of_samples):
+    image = images[i]
+    image.save(f"{outpath}_{i}.png")
 
 with open(f"{outpath}.txt", 'w', encoding='UTF-8') as f:
     f.write(prompt)
--- src/main/java/org/langrid/mlgridservices/service/impl/StableDiffusionTextImageGenerationService.java
+++ src/main/java/org/langrid/mlgridservices/service/impl/StableDiffusionTextImageGenerationService.java
@@ -6,13 +6,14 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.ArrayList;
 
+import org.langrid.mlgridservices.util.FileUtil;
 import org.langrid.mlgridservices.util.GPULock;
 import org.langrid.mlgridservices.util.LanguageUtil;
+import org.langrid.mlgridservices.util.ProcessUtil;
 import org.langrid.service.ml.TextToImageGenerationResult;
 import org.langrid.service.ml.TextToImageGenerationService;
 import org.springframework.stereotype.Service;
 
-import jp.go.nict.langrid.commons.io.FileUtil;
 import jp.go.nict.langrid.service_1_2.InvalidParameterException;
 import jp.go.nict.langrid.service_1_2.ProcessFailedException;
 import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
@@ -33,11 +34,17 @@ public class StableDiffusionTextImageGenerationService implements TextToImageGen
 			maxResults = Math.min(maxResults, 8);
 			var tempDir = new File(baseDir, "temp");
 			tempDir.mkdirs();
-			var temp = FileUtil.createUniqueFile(tempDir, "outimage-", "");
-			run(text, maxResults, "temp/" + temp.getName());
+			var temp = FileUtil.createUniqueFileWithDateTime(tempDir, "out-", "");
+			var cmd = String.format(
+					"PATH=$PATH:/usr/local/bin " +
+					"/usr/local/bin/docker-compose run --rm service " +
+					"python3 run.py \"%s\" %d temp/%s",
+					text.replaceAll("\"", "\\\""), maxResults, temp.getName());
+			System.out.println(cmd);
+			ProcessUtil.runAndWait(cmd, baseDir);
 			var ret = new ArrayList<TextToImageGenerationResult>();
 			for(var i = 0; i < maxResults; i++){
-				var imgFile = new File(tempDir, temp.getName() + ".png"); //"_" + i + ".png");
+				var imgFile = new File(temp.toString() + "_" + i + ".png");
 				if(!imgFile.exists()) break;
 //				var accFile = new File(tempDir, temp.getName() + "_" + i + ".acc.txt");
 				ret.add(new TextToImageGenerationResult(
@@ -50,38 +57,6 @@ public class StableDiffusionTextImageGenerationService implements TextToImageGen
 		} catch(RuntimeException e) {
 			throw e;
 		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public void run(String text, int maxResults, String outimagePrefix){
-		try(var l = GPULock.acquire()){
-			var cmd = "PATH=$PATH:/usr/local/bin /usr/local/bin/docker-compose " +
-					"run --rm sd python3 run.py " +
-				" \"" + text.replaceAll("\"", "\\\"") + "\" " +
-				" " + outimagePrefix;
-			System.out.println(cmd);
-			var pb = new ProcessBuilder("bash", "-c", cmd);
-			pb.directory(baseDir);
-			var proc = pb.start();
-			try {
-				proc.waitFor();
-				var res = proc.exitValue();
-				if(res == 0) {
-					return;
-				} else {
-					var br = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-					var lines = new StringBuilder();
-					String line = null;
-					while ((line = br.readLine()) != null) {
-						lines.append(line);
-					}
-					throw new RuntimeException(lines.toString());
-				}
-			} finally {
-				proc.destroy();
-			}
-		} catch(Exception e){
 			throw new RuntimeException(e);
 		}
 	}
