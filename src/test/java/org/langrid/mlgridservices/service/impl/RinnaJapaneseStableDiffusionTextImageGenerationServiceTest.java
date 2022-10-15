08a0c2b4fe7c4de3b899f6e6d1d6327b39b4313d
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-10-15 16:12:20 +0900

    コンパイルエラー修正

--- src/test/java/org/langrid/mlgridservices/service/impl/RinnaJapaneseStableDiffusionTextImageGenerationServiceTest.java
+++ src/test/java/org/langrid/mlgridservices/service/impl/RinnaJapaneseStableDiffusionTextImageGenerationServiceTest.java
@@ -6,6 +6,6 @@ public class RinnaJapaneseStableDiffusionTextImageGenerationServiceTest {
 	@Test
 	public void test() throws Throwable{
 		var ig = new RinnaJapaneseStableDiffusionTextImageGenerationService();
-		ig.generate("ja", "巨漢の男が三毛猫を追い回すアニメ", "image/png", 2);
+		ig.generate("ja", "巨漢の男が三毛猫を追い回すアニメ");
 	}
 }
