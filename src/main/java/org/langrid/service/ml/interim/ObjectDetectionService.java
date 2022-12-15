c1cad56efd5596f480b4c49485312d3cc53817d5
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-12-15 10:16:10 +0900

    オブジェクト検出の新インタフェース

---
+++ src/main/java/org/langrid/service/ml/interim/ObjectDetectionService.java
@@ -0,0 +1,11 @@
+package org.langrid.service.ml.interim;
+
+import jp.go.nict.langrid.service_1_2.InvalidParameterException;
+import jp.go.nict.langrid.service_1_2.ProcessFailedException;
+import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
+
+public interface ObjectDetectionService {
+	ObjectDetectionResult[] detect(
+			byte[] image, String imageFormat, String labelLanguage)
+	throws InvalidParameterException, ProcessFailedException, UnsupportedLanguageException;
+}
