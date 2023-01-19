82c25b1061166a4a5bec4edb3e8ef3cccdb20f12
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2023-01-20 08:41:06 +0900

    reasonspeech

--- procs/reazonspeech/run.py
+++ procs/reazonspeech/run.py
@@ -1,8 +1,22 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-from huggingface_hub import notebook_login
-notebook_login()
+
+import os
+
+TOKEN = os.environ['HUGGINGFACE_TOKEN']
+
+from huggingface_hub import login
+login(TOKEN)
+
+
+import argparse, os
+parser = argparse.ArgumentParser()
+parser.add_argument("inputFilePath",type=str, default="temp/test_ja_16k.wav")
+args = parser.parse_args()
+inputFilePath = args.inputFilePath
+outputFilePath = f"{inputFilePath}.out.txt"
+
 
 import torch
 from espnet2.bin.asr_inference import Speech2Text
@@ -10,7 +24,10 @@ from espnet2.bin.asr_inference import Speech2Text
 device = "cuda" if torch.cuda.is_available() else "cpu"
 beam_size = 5#@param {type:"integer"}
 
+#from pathlib import Path
+
 reazonspeech = Speech2Text.from_pretrained(
+  #"/opt/conda/lib/python3.7/site-packages/espnet_model_zoo/models--reazon-research--reazonspeech-espnet-v1/snapshots/42f0366852e9ff23af808ccff368f0bfb9038809/",
   "reazon-research/reazonspeech-espnet-v1",
   beam_size=beam_size,
   batch_size=0,
@@ -18,20 +35,8 @@ reazonspeech = Speech2Text.from_pretrained(
 )
 
 import librosa
-import librosa.display
-from IPython.display import display, Audio
-import matplotlib.pyplot as plt
-from datetime import datetime
-def display_speech(speech, sample_rate):
-  display(Audio(speech, rate=sample_rate))
-  librosa.display.waveshow(speech, sr=sample_rate)
-  plt.show()
 
-def transcribe(speech):
-  asr_results = reazonspeech(speech)
-  print(asr_results[0][0])
+speech, sample_rate = librosa.load(inputFilePath)
 
-
-speech, sample_rate = librosa.load(file_name, sr=sample_rate)
-
-print(transcribe(speech))
+with open(outputFilePath, "w") as f:
+  f.write(reazonspeech(speech)[0][0])
