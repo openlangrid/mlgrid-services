09f8bfd00b680afca8f127d58e88c9956f48079d
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2023-01-23 15:08:38 +0900

    引数追加

--- procs/reazonspeech/run.sh
+++ procs/reazonspeech/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker-compose run --rm service python run.py "$@"
+OUT_FILE_NAME=${1:-sample/test_ja_16k.wav}
+shift
+
+docker-compose run --rm service python run.py "$OUT_FILE_NAME" "$@"
