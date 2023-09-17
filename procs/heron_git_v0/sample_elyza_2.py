e5a24befb0444d61355b5b320e0455fdb4565dbf
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2023-09-18 00:44:49 +0900

    add sample scripts

---
+++ procs/heron_git_v0/sample_elyza_2.py
@@ -0,0 +1,46 @@
+import requests
+from PIL import Image
+
+import torch
+from transformers import AutoProcessor
+from heron.models.git_llm.git_llama import GitLlamaForCausalLM
+
+device_id = 0
+
+# prepare a pretrained model
+model = GitLlamaForCausalLM.from_pretrained(
+    'turing-motors/heron-chat-git-ELYZA-fast-7b-v0', torch_dtype=torch.float16
+)
+model.eval()
+model.to(f"cuda:{device_id}")
+
+# prepare a processor
+processor = AutoProcessor.from_pretrained('turing-motors/heron-chat-git-ELYZA-fast-7b-v0')
+
+# prepare inputs
+url = "https://www.barnorama.com/wp-content/uploads/2016/12/03-Confusing-Pictures.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+text = f"##human: これは何の写真ですか？\n##gpt: "
+
+# do preprocessing
+inputs = processor(
+    text,
+    image,
+    return_tensors="pt",
+    truncation=True,
+)
+inputs = {k: v.to(f"cuda:{device_id}") for k, v in inputs.items()}
+
+# set eos token
+eos_token_id_list = [
+    processor.tokenizer.pad_token_id,
+    processor.tokenizer.eos_token_id,
+]
+
+# do inference
+with torch.no_grad():
+    out = model.generate(**inputs, max_length=256, do_sample=False, temperature=0., eos_token_id=eos_token_id_list)
+
+# print result
+print(processor.tokenizer.batch_decode(out)[0])
