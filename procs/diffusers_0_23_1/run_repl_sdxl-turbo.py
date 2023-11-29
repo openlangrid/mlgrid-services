dd7add4050accbad6a33da88e5fca7356baf3fae
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2023-11-29 23:47:14 +0900

    sdxl-turbo追加

---
+++ procs/diffusers_0_23_1/run_repl_sdxl-turbo.py
@@ -0,0 +1,38 @@
+def run(model_name: str):
+    from diffusers import AutoPipelineForText2Image
+    import torch
+    pipe = AutoPipelineForText2Image.from_pretrained(
+        model_name, 
+        torch_dtype=torch.float16, 
+        variant="fp16",
+        device_map="auto")
+    print("ready", flush=True)
+
+    import json, sys
+    for line in sys.stdin:
+        input = json.loads(line)
+        with open(input["promptPath"]) as f:
+            prompt = f.read()
+        language = input["promptLanguage"]
+        numberOfTimes = input["numberOfTimes"]
+        outputPathPrefix = input["outputPathPrefix"]
+        for i in range(numberOfTimes):
+            image = pipe(
+                prompt=prompt, 
+                num_inference_steps=1, 
+                guidance_scale=0.0
+                ).images[0]
+            image.save(f"{outputPathPrefix}_{i}.png")
+        print("ok", flush=True)
+
+
+def main(model: str):
+    run(model)
+
+
+if __name__ == "__main__": 
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", type=str, nargs="?",
+        default="stabilityai/sdxl-turbo")
+    main(**vars(parser.parse_args()))
---
+++ procs/diffusers_0_23_1/run_sample_sdxl-turbo.py
@@ -0,0 +1,22 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+
+# パイプラインの準備
+pipe = AutoPipelineForText2Image.from_pretrained(
+    "stabilityai/sdxl-turbo", 
+    torch_dtype=torch.float16, 
+    variant="fp16",
+    device_map="auto"
+)
+
+prompt = "cute cat ear maid"
+
+# 画像生成
+image = pipe(
+    prompt=prompt, 
+    num_inference_steps=1, 
+    guidance_scale=0.0
+).images[0]
+
+image.save("sample/sdxl-turbo.png")
+
