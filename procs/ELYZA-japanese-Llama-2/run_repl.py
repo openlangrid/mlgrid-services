8c2c0c8743a7d3a825d5618c9f143f12c4f7584d
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-01-16 00:39:33 +0900

    複数サービスタイプに対応した実行スクリプト追加

---
+++ procs/ELYZA-japanese-Llama-2/run_repl.py
@@ -0,0 +1,86 @@
+bfloat16Types = set(["elyza/ELYZA-japanese-Llama-2-13b-fast-instruct",
+                    "elyza/ELYZA-japanese-Llama-2-13b-instruct"])
+
+def run(tokenizer_model_name: str, model_name: str):
+    import torch
+    from torch.cuda import OutOfMemoryError
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_model_name)
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            torch_dtype=torch.bfloat16 if model_name in bfloat16Types else torch.float16,
+            device_map="cuda", use_cache=True, low_cpu_mem_usage=True)
+        model.eval()
+        print("ready", flush=True)
+
+        import json, sys
+        for line in sys.stdin:
+            input = json.loads(line)
+            serviceType = input["serviceType"]
+            methodName = input["methodName"]
+            outputPath = input["outputPath"]
+            if serviceType == "TextGenerationService" and methodName == "generate":
+                with open(input["textPath"]) as f:
+                    text = f.read()
+                language = input["textLanguage"]
+                sourceText = text
+            elif serviceType == "TextInstructionService" and methodName == "generate":
+                systemPrompt = None
+                if "systemPromptPath" in input:
+                    fname = input["systemPromptPath"]
+                    if len(fname) > 0:
+                        with open(fname) as f:
+                            systemPrompt = f.read()
+                if systemPrompt == None or len(systemPrompt) == 0:
+                    systemPrompt = "あなたは誠実で優秀な日本人のアシスタントです。"
+                with open(input["userPromptPath"]) as f:
+                    userPrompt = f.read()
+                promptLanguage = input["promptLanguage"]
+                sourceText = f"{tokenizer.bos_token}[INST] <<SYS>>\n{systemPrompt}\n<</SYS>>{userPrompt} [/INST]"
+            elif serviceType == "QuestionAnsweringService" and methodName == "ask":
+                question = None
+                context = None
+                with open(input["questionPath"]) as f:
+                    question = f.read()
+                with open(input["contextPath"]) as f:
+                    context = f.read()
+                language = input["language"]
+                systemPrompt = "<<SYS>>\n参考情報を元に、質問にできるだけ正確に答えてください。\n<</SYS>>\n\n"
+                contextAndQuestion = f"{context}\n質問は次のとおりです。{question}"
+                sourceText = f"{tokenizer.bos_token}[INST] {systemPrompt}{contextAndQuestion} [/INST] "
+            token_ids = tokenizer.encode(
+                sourceText, add_special_tokens=False, return_tensors="pt"
+                ).to(model.device)
+            with torch.no_grad():
+                output_ids = model.generate(
+                    token_ids,
+                    max_new_tokens=512, do_sample=True, top_p=0.95, temperature=0.7,
+                    pad_token_id=tokenizer.pad_token_id,
+                    eos_token_id=tokenizer.eos_token_id,
+                )
+            output = tokenizer.decode(
+                output_ids.tolist()[0][token_ids.size(1) :],
+                skip_special_tokens=True)
+            with open(outputPath, mode="w") as f:
+                f.write(output)
+            from gpuinfo import get_gpu_properties
+            props = get_gpu_properties()
+            print(f"ok {json.dumps(props)}", flush=True)
+    except OutOfMemoryError:
+        print("ng torch.cuda.OutOfMemoryError", flush=True)
+    except RuntimeError:  # RuntimeError: "addmm_impl_cpu_" not implemented for 'Half'
+        print("ng RuntimeError", flush=True)
+
+
+def main(tokenizerModel: str, model: str):
+    run(tokenizerModel if tokenizerModel is not None else model
+        , model)
+
+
+if __name__ == "__main__": 
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", type=str, nargs="?", default="elyza/ELYZA-japanese-Llama-2-7b-instruct")
+    parser.add_argument("--tokenizerModel", type=str, default=None)
+    main(**vars(parser.parse_args()))
