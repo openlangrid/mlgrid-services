363897150d03ce02bbd6e0f9f2f0c982659793f4
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-02-04 12:45:35 +0900

    モデルダウンロードスクリプト更新

--- procs/llamacpp/download_models.sh
+++ procs/llamacpp/download_models.sh
@@ -1,15 +1,46 @@
+#!/bin/bash
+set -eu
 
-if [ ! -e ./models/karakuri-lm-70b-chat-v0.1-q5_K_S.gguf ]; then
-  wget https://huggingface.co/mmnga/karakuri-lm-70b-chat-v0.1-gguf/resolve/main/karakuri-lm-70b-chat-v0.1-q5_K_S.gguf?download=true \
-    -O ./models/karakuri-lm-70b-chat-v0.1-q5_K_S.gguf
-fi
+declare -A models=(
+  ["karakuri-lm-70b-chat-v0.1-gguf"]="
+    karakuri-lm-70b-chat-v0.1-q5_K_S.gguf
+  "
+  ["ELYZA-japanese-Llama-2-13b-fast-instruct-gguf"]="
+    ELYZA-japanese-Llama-2-13b-fast-instruct-q5_K_M.gguf
+    ELYZA-japanese-Llama-2-13b-fast-instruct-q5_K_S.gguf
+  "
+  ["ELYZA-japanese-Llama-2-7b-fast-instruct-gguf"]="
+    ELYZA-japanese-Llama-2-7b-fast-instruct-q5_K_M.gguf
+    ELYZA-japanese-Llama-2-7b-fast-instruct-q5_K_S.gguf
+  "
+  ["cyberagent-calm2-7b-chat-dpo-experimental-gguf"]="
+    cyberagent-calm2-7b-chat-dpo-experimental-q5_K_S.gguf
+  "
+  ["rinna-nekomata-14b-instruction-gguf"]="
+    rinna-nekomata-14b-instruction-q5_K_M.gguf
+    rinna-nekomata-14b-instruction-q5_K_S.gguf
+  "
+  ["rinna-nekomata-7b-instruction-gguf"]="
+    rinna-nekomata-7b-instruction-q5_K_M.gguf
+    rinna-nekomata-7b-instruction-q5_K_S.gguf
+  "
+  ["rinna-youri-7b-instruction-gguf"]="
+    rinna-youri-7b-instruction-q5_K_M.gguf
+    rinna-youri-7b-instruction-q5_K_S.gguf
+  "
+  ["rinna-youri-7b-chat-gguf"]="
+    rinna-youri-7b-chat-q5_K_M.gguf
+    rinna-youri-7b-chat-q5_K_S.gguf
+  "
+)
 
-if [ ! -e ./models/ELYZA-japanese-Llama-2-13b-fast-instruct-q5_K_S.gguf ]; then
-  wget https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-13b-fast-instruct-gguf/resolve/main/ELYZA-japanese-Llama-2-13b-fast-instruct-q5_K_S.gguf?download=true \
-    -O ./models/ELYZA-japanese-Llama-2-13b-fast-instruct-q5_K_S.gguf
-fi
+for D in "${!models[@]}"; do
+  MS=(`echo ${models[${D}]}`)
+  for M in "${MS[@]}"; do
+    if [ ! -e ./models/${M} ]; then
+      wget https://huggingface.co/mmnga/${D}/resolve/main/${M}?download=true \
+        -O ./models/${M}
+    fi
+  done
+done
 
-if [ ! -e ./models/cyberagent-calm2-7b-chat-dpo-experimental-q5_K_S.gguf ]; then
-  wget https://huggingface.co/mmnga/cyberagent-calm2-7b-chat-dpo-experimental-gguf/resolve/main/cyberagent-calm2-7b-chat-dpo-experimental-q5_K_S.gguf?download=true \
-    -O ./models/cyberagent-calm2-7b-chat-dpo-experimental-q5_K_S.gguf
-fi
