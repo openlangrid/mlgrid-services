3d200926389e3ccbfe6f4df2905d18b5fc478ecc
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-04-22 22:52:55 +0900

    不要なimportを削除

--- src/main/java/org/langrid/service/ml/interim/TextGuidedVideoGenerationService.java
+++ src/main/java/org/langrid/service/ml/interim/TextGuidedVideoGenerationService.java
@@ -1,7 +1,5 @@
 package org.langrid.service.ml.interim;
 
-import org.langrid.service.ml.Image;
-
 import jp.go.nict.langrid.service_1_2.InvalidParameterException;
 import jp.go.nict.langrid.service_1_2.ProcessFailedException;
 import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
