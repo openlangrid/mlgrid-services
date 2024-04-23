385292b8f0eb6161cd2049189bae401fb21d48c5
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-04-23 14:04:36 +0900

    commandrplusが動かなかったのを修正

---
+++ procs/llamacpp_v0.2.57/test_commandrplus_Q4_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ORIG_MODEL_REPO=CohereForAI/c4ai-command-r-plus
+GGUF_MODEL_REPO=pmysl/c4ai-command-r-plus-GGUF
+GGUF_MODEL_PATTERN="command-r-plus-Q4_K_M-00002-of-00002.gguf"
+docker compose run -T --rm service python test.py $ORIG_MODEL_REPO $GGUF_MODEL_REPO $GGUF_MODEL_PATTERN
+
