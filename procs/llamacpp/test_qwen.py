fb5d916539c1aa7cded5246c8d7347a7c3ce7337
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-01-16 23:03:35 +0900

    動かないモデルを無効化

--- procs/llamacpp/test_qwen.py
+++ procs/llamacpp/test_qwen.py
@@ -1,8 +1,10 @@
 from llama_cpp import Llama
 
 llm = Llama(
-    model_path="./models/qwen1_5-72b-chat-q4_k_m.gguf",
-    n_gpu_layers=-1)
+#    model_path="./models/qwen1_5-72b-chat-q4_k_m.gguf",
+#    n_gpu_layers=-1)
+    model_path="./models/qwen1_5-72b-chat-q5_k_m.gguf",
+    n_gpu_layers=78)
 prompt="<|im_start|>user\nまどか☆マギカで誰が一番かわいい？その理由も説明して<|im_end|>\n<|im_start|>assistant\n"
 output = llm(prompt, max_tokens=512)
 print(output["choices"][0]["text"])
